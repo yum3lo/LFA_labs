22ef7a9da7614ac245a2d7a057010361d6377602
maria <118808220+yum3lo@users.noreply.github.com>
2024-03-04 11:31:58 +0200

    graph

--- lab1/finite_automaton_class.py
+++ lab1/finite_automaton_class.py
@@ -37,24 +37,3 @@ class FiniteAutomaton:
 
         # Check if the final state after processing all symbols is one of the accept states
         return current_state in self.accept_states
-    
-# Define the finite automaton
-Q = {'q0', 'q1', 'q2', 'q3', 'q4'}
-Sigma = {'a', 'b'}
-F = {'q3'}
-delta = {
-    'q0': {'a': 'q1'},
-    'q1': {'b': 'q2'},
-    'q2': {'b': 'q0', 'a': 'q3'},
-    'q3': {'a': 'q4'},
-    'q4': {'a': 'q0'},
-}
-
-# Create an instance of the FiniteAutomaton class
-finite_automaton = FiniteAutomaton(Q, Sigma, delta, 'q0', F)
-
-# Check if the finite automaton is deterministic
-if finite_automaton.is_deterministic():
-    print("The finite automaton is deterministic.")
-else:
-    print("The finite automaton is nondeterministic.")
